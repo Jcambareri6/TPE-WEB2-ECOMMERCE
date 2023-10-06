7ae5e598119185ec2c69a88ca4d185abb43002c9
Jcambareri6 <joaquin.cambareri@eest3necochea.edu.ar>
2023-10-06 10:05:01 -0300

    prueba

--- App/Models/model.php
+++ App/Models/model.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once './Models/config.php';
+require_once './app/Models/config.php';
 class DB{
    
     private $db;
@@ -9,5 +9,4 @@ class DB{
         $this->db= new PDO("mysql:host=". DB_HOST . ";dbname=". DB_NAME .";charset=" .DB_Charset,DB_USER, DB_PASS );
      
     }
-
 }
