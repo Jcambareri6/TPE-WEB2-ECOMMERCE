762a3a8d6ea171f4e8d41b64be656ed22e5685d8
Bruno De la Penna <bdelapenna@gmail.com>
2023-10-06 11:57:22 -0300

    .

--- App/Models/Auth.model.php
+++ App/Models/Auth.model.php
@@ -1,12 +1,6 @@
 <?php 
 require_once './App/Models/model.php';
 class UserModel extends DB {
-    private $db;
-
-    function __construct() {
-        $this->db = new DB();
-        
-    }
 
     public function getByEmail($email) {
         $query = $this->connect()->prepare('SELECT * FROM usuarios WHERE email = ?');
