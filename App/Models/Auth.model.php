a7d872f362f003da685bc75675216d72894c3a71
Jcambareri6 <joaquin.cambareri@eest3necochea.edu.ar>
2023-10-06 11:56:22 -0300

    login realizado

--- App/Models/Auth.model.php
+++ App/Models/Auth.model.php
@@ -1,15 +1,9 @@
 <?php 
-require_once './App/Models/model.php';
+require_once'./App/Models/model.php';
 class UserModel extends DB {
-    private $db;
-
-    function __construct() {
-        $this->db = new DB();
-        
-    }
 
     public function getByEmail($email) {
-        $query = $this->connect()->prepare('SELECT * FROM usuarios WHERE email = ?');
+        $query = $this->connect()->prepare('SELECT * FROM usuarios WHERE username = ?');
         $query->execute([$email]);
 
         return $query->fetch(PDO::FETCH_OBJ);
